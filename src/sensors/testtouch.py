225a634438f838384423d9869ff33cfe2f628f3c
JeremieLapointe13 <2141363@cegeprdl.ca>
2025-04-10 16:59:24 -0400

    123

--- src/sensors/testtouch.py
+++ src/sensors/testtouch.py
@@ -6,13 +6,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(touch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def touch_det(pin):
-    return not GPIO.input(pin)
+    return GPIO.input(pin)
 
 try:
     while True:
-        if touch_det(touch_pin): 
-            print ('['+time.ctime()+'] - '+'Touch Detected')
+        state = touch_det(touch_pin)
+        print(f'[{time.ctime()}] - État du bouton: {state}')
         time.sleep(0.2)
 except KeyboardInterrupt:
-  print('interrupted!')
-  GPIO.cleanup()
+    print('interrupted!')
+    GPIO.cleanup()
