f7f00cd91be59424f714595bd1e24f3a89822841
JeremieLapointe13 <2141363@cegeprdl.ca>
2025-04-10 16:49:55 -0400

    test touch

--- src/sensors/testtouch.py
+++ src/sensors/testtouch.py
@@ -1,31 +1,18 @@
 import time
 import RPi.GPIO as GPIO
 
-# Configuration du capteur tactile TP223
 touch_pin = 17
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(touch_pin, GPIO.IN) 
+GPIO.setup(touch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Fonction de détection du toucher
 def touch_det(pin):
-    return GPIO.input(pin)  # Retourne 1 quand touché, 0 sinon
-
-print("Test du capteur tactile TP223 - Appuyez sur CTRL+C pour quitter")
-print("En attente de touches...")
-
-etat_precedent = False
+    touch=GPIO.input(pin)
+    return touch
 
 try:
     while True:
-        etat_actuel = touch_det(touch_pin)
-        
-        # Détection du changement d'état (front montant)
-        if etat_actuel and not etat_precedent:
-            print('['+time.ctime()+'] - Touch Detected')
-        
-        etat_precedent = etat_actuel
-        time.sleep(0.1)  # Délai réduit pour une meilleure réactivité
-
+        if touch_det(touch_pin): print ('['+time.ctime()+'] - '+'Touch Detected')
+        time.sleep(0,2)
 except KeyboardInterrupt:
-    print('Programme interrompu!')
-    GPIO.cleanup()
+  print('interrupted!')
+  GPIO.cleanup()
